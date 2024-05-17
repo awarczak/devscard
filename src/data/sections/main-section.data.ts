40cfbdafb805366d70f6536d55ac99c236273c3b
Ada <adrianna.warczak@zaven.pl>
2024-05-17 18:28:17 +0200

    Add checkbox

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -24,13 +24,10 @@ const mainSectionData = {
     { label: 'LinkedIn', value: '/in/adrianna-warczak', url: 'https://linkedin.com' },
     // { label: 'Website', value: 'mark-freeman-personal-website.com', url: '/', fullRow: true },
   ],
-  description: `I've been creating web applications for <b>6 years</b>. I’m experienced in developing frontend applications using various technical stacks within the React.js ecosystem. For most of my career, I have been working with <b>greenfield/early-stage products</b> with a high level of business-developer collaboration. Outside of my commercial work, I actively engage in <b>non-profit organizations</b> related to web development, participate in conferences, and join hackathons. Some of the achievements in these areas that I am proudest of are listed in the sections below. Currently, I’m looking for a project where I can utilize my knowledge of <b>modern front-end architecture</b> and <b>DDD</b> to build something with a profound impact on people’s lives.`,
-  tags: [
-    {
-      icon: 'material-symbols:check-box-sharp',
-      name: 'I commit to support and follow the rules established by The Remote Manifesto',
-    },
-  ],
+  description: `I've been creating web applications for <b>6 years</b>. I’m experienced in developing frontend applications using various technical stacks within the React.js ecosystem. For most of my career, I have been working with <b>greenfield/early-stage products</b> with a high level of business-developer collaboration. Outside of my commercial work, I actively engage in <b>non-profit organizations</b> related to web development, participate in conferences, and join hackathons. Some of the achievements in these areas that I am proudest of are listed in the sections below. Currently, I’m looking for a project where I can utilize my knowledge of <b>modern front-end architecture</b> and <b>DDD</b> to build something with a profound impact on people’s lives.
+<br><input type="checkbox" id="checkbox" checked disabled /> I commit to support and follow the rules established by The Remote Manifesto.
+  `,
+  tags: [],
   action: {
     label: 'Download CV',
     url: '/cv.pdf',
